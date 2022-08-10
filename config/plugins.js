4938061ff6a85275d5c7e8fb5bc9e9c62fdce788
Matt Coleman <matt.coleman@protonmail.com>
2022-08-10 20:33:48 +0100

    chore: added send grid

--- config/plugins.js
+++ config/plugins.js
@@ -2,7 +2,20 @@ module.exports = ({ env }) => ({
   "import-export-entries": {
     enabled: true,
     config: {
-      serverPublicHostname: "https://pure-atoll-11674.herokuapp.com/",
+      serverPublicHostname: env("SERVER_PUBLIC_HOSTNAME"),
+    },
+  },
+  email: {
+    config: {
+      provider: "sendgrid",
+      providerOptions: {
+        apiKey: env("SENDGRID_API_KEY"),
+      },
+      settings: {
+        defaultFrom: env("EMAIL_DEFAULT_FROM"),
+        defaultReplyTo: env("EMAIL_DEFAULT_REPLY_TO"),
+        testAddress: env("EMAIL_TEST_ADDRESS"),
+      },
     },
   },
 });
