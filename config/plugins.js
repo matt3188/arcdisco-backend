163de907a65ef69b0708ac002ab8168ea3c7464e
Matt Coleman <matt.coleman@protonmail.com>
2022-08-10 18:33:44 +0100

    chore: added strapi-plugin-import-export-entries

---
+++ config/plugins.js
@@ -0,0 +1,8 @@
+module.exports = ({ env }) => ({
+  "import-export-entries": {
+    enabled: true,
+    config: {
+      serverPublicHostname: "https://pure-atoll-11674.herokuapp.com/",
+    },
+  },
+});
